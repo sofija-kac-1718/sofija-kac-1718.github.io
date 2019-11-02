2a4360fe7ec53d8086b7828e6865659274240a0b
Sofija <sofijakac@gmail.com>
2019-11-02 11:04:10 -0700

    rps

--- CS160/processing/project2/sketch.js
+++ CS160/processing/project2/sketch.js
@@ -1,10 +1,12 @@
 function setup() {
-  createCanvas(800,600)
-
+  createCanvas(500,500);
 }
 
 function draw() {
-  if (mouseIsPressed) {
-    
+  if (mousePresses){
+    fill(0)
+  } else {
+    fill(255);
   }
+  ellipse(mouseX, mouseY, 80, 80)
 }
